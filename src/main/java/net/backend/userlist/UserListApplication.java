470d4c80a150a399e6723e920c72023908ba86ee
Josué Teixeira <zjosuez@outlook.com>
2022-09-21 18:33:10 -0300

    Initial Commit

---
+++ src/main/java/net/backend/userlist/UserListApplication.java
@@ -0,0 +1,13 @@
+package net.backend.userlist;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UserListApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(UserListApplication.class, args);
+    }
+
+}
---
+++ src/test/java/net/backend/userlist/UserListApplicationTests.java
@@ -0,0 +1,13 @@
+package net.backend.userlist;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UserListApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
