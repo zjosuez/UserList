f8f5f321d7ecd558d59efed25eeb1faf09e19c14
Josué Teixeira <zjosuez@outlook.com>
2022-09-22 12:46:48 -0300

    Integração com o front end

--- angular-frontend/src/app/user.service.ts
+++ angular-frontend/src/app/user.service.ts
@@ -1,9 +1,17 @@
 import { Injectable } from '@angular/core';
-
+import { HttpClient} from '@angular/common/http';
+import { Observable } from 'rxjs';
+import { User } from './user';
 @Injectable({
   providedIn: 'root'
 })
 export class UserService {
 
-  constructor() { }
+  private baseUrl = 'http://localhost:8080/api/users';
+
+  constructor(private http: HttpClient) { }
+
+  getUsers(): Observable<User[]>{
+    return this.http.get<User[]>(`${this.baseUrl}`);
+  }
 }
